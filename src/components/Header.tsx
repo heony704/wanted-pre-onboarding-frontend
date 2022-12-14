7edacc1c846fad3c48fdf4aab0fafb12be55bb6d
heony704 <heony704@gmail.com>
2022-12-14 21:58:52 +0900

    이름변경

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,7 +3,7 @@ import styled from "styled-components";
 export default function Header() {
     return (
         <Container>
-            <span>todolist</span>
+            <span>Todolist</span>
         </Container>
     );
 }
