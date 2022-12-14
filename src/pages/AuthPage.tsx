98551e59941e8271bded15682cca6451bf8071a5
heony704 <heony704@gmail.com>
2022-12-14 18:40:07 +0900

    토큰 상태에 따른 리다이렉트

--- src/pages/AuthPage.tsx
+++ src/pages/AuthPage.tsx
@@ -1,8 +1,17 @@
 import styled from 'styled-components';
 import Header from '../components/Header';
 import SignForm from '../components/SignForm';
+import { useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
 
 export default function AuthPage() {
+    const navigate = useNavigate();
+    useEffect(() => {
+        if (localStorage.getItem("access_token"))  {
+            navigate('/todo');
+        }
+    });
+
     return (
         <Container>
             <Header />
--- src/pages/TodoListPage.tsx
+++ src/pages/TodoListPage.tsx
@@ -1,7 +1,16 @@
 import styled from 'styled-components';
 import Header from '../components/Header';
+import { useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
 
 export default function TodoListPage() {
+    const navigate = useNavigate();
+    useEffect(() => {
+        if (!localStorage.getItem("access_token"))  {
+            navigate('/');
+        }
+    });
+
     return (
         <Container>
             <Header />
