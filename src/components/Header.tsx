063a6fa0fa055c7eacf7f559d281221fc4ecca7f
heony704 <heony704@gmail.com>
2022-12-14 17:40:47 +0900

    header 추가

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,3 +1,21 @@
+import styled from "styled-components";
+
 export default function Header() {
-    return (<div></div>);
+    return (
+        <Container>
+            <span>todolist</span>
+        </Container>
+    );
 }
+
+const Container = styled.div`
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    width: 100%;
+    height: 70px;
+    
+    user-select: none;
+    font-size: 1.5rem;
+    font-weight: 700;
+`;
--- src/pages/TodoListPage.tsx
+++ src/pages/TodoListPage.tsx
@@ -1,5 +1,16 @@
 import styled from 'styled-components';
+import Header from '../components/Header';
 
 export default function TodoListPage() {
-    return (<div></div>);
+    return (
+        <Container>
+            <Header />
+        </Container>
+    );
 }
+
+const Container = styled.div`
+    min-height: 100vh;
+    max-width: 50rem;
+    margin: 0 auto;
+`;
